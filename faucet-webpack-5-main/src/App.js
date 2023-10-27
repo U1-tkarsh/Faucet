d390f6463120b28cb991a36f290a244872715442
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-10-27 14:00:34 +0530

    Load Balance error

--- faucet-webpack-5-main/src/App.js
+++ faucet-webpack-5-main/src/App.js
@@ -1,77 +1,86 @@
-import './App.css';
-import React, {useEffect, useState} from 'react';
-import Web3 from 'web3';
-import detectEthereumProvider from '@metamask/detect-provider';
-import { loadContract } from './utils/load-contract';
+
+import { useEffect, useState } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectEthereumProvider from '@metamask/detect-provider'
+import { loadContract } from "./utils/load-contract";
+
 
 function App() {
-
   const [web3Api, setWeb3Api] = useState({
     provider: null,
     web3: null,
     contract: null
   })
 
+  const [balance, setBalance] = useState(null)
   const [account, setAccount] = useState(null)
 
   useEffect(() => {
     const loadProvider = async () => {
-      // with metamask we have access to window.ethereum & to window.web3
-      // metamask injects a global API into website
-      // this API will allow website to request users, accounts, read data to blockchain
-      // can also sign messages and transactions
-
       const provider = await detectEthereumProvider()
-      const contract = await loadContract('Faucet', provider)
+      const contract = await loadContract("Faucet", provider)
 
-      if(provider) {
+      if (provider) {
         setWeb3Api({
           web3: new Web3(provider),
           provider,
           contract
         })
       } else {
-        console.error('Please install Metamask')
+        console.error("Please, install Metamask.")
       }
     }
 
-    loadProvider ()
+    loadProvider()
   }, [])
 
   useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api
+      const balance = await web3.eth.getBalance(contract.address)
+      setBalance(web3.utils.fromWei(balance, "ether"))
+    }
+
+    web3Api.contract && loadBalance()
+  }, [web3Api])  
+
+  useEffect(() => {
     const getAccount = async () => {
-      const accounts = await web3Api.web3.eth.getAccounts()
-      setAccount(accounts[0])
+      web3Api.web3.eth.getAccounts((error, accounts) => {
+        accounts && setAccount(accounts[0])
+      });
     }
     web3Api.web3 && getAccount()
-  }, [web3Api.web3])
+  }, [web3Api.web3]);
 
   return (
     <>
-      <div className = 'faucet-wrapper'>
-        <div className = 'faucet'>
-          <div className = 'is-flex is-align-items-center'>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="is-flex is-align-items-center">
             <span>
-              <strong className = 'mr-2'>Account: </strong>
+              <strong className="mr-2">Account: </strong>
             </span>
-            
-              {account ? 
-              <div>{account}</div>:
-              <button
-              className = 'button is-small'
-              onClick = {() => web3Api.provider.request({method: 'eth_requestAccounts'}
-              )}
-              >
-                Connect Wallet
-              </button>
+              { account ?
+                <div>{account}</div> :
+                <button
+                  className="button is-small"
+                  onClick={() =>
+                    web3Api.provider.request({method: "eth_requestAccounts"}
+                  )}
+                >
+                  Connect Wallet
+                </button>
               }
           </div>
-          <div className = 'balance-view is-size-2 my-4'>
-            Current Balance: <strong>10</strong> ETH
+          <div className="balance-view is-size-2 my-4">
+            Current Balance: <strong>{balance}</strong> ETH
           </div>
-          
-          <button className = 'button is-link mr-2'>Donate</button>
-          <button className = 'button is-primary is-dark'>Withdraw</button>
+          <button
+            className="button is-link mr-2">Donate</button>
+          <button
+            className="button is-primary">Withdraw</button>
         </div>
       </div>
     </>
