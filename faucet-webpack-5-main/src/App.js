511cec10d1f8c86f42662098d86745445bc3d74f
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-10-27 15:43:07 +0530

    update web3 package, and add withdraw and add fund func

--- faucet-webpack-5-main/src/App.js
+++ faucet-webpack-5-main/src/App.js
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
 import detectEthereumProvider from '@metamask/detect-provider'
@@ -13,8 +13,11 @@ function App() {
     contract: null
   })
 
-  const [balance, setBalance] = useState(null)
+  const [balance, setBallance] = useState(null)
   const [account, setAccount] = useState(null)
+  const [shouldReload, reload] = useState(false)
+
+  const reloadEffect = useCallback(() => reload(!shouldReload), [shouldReload])
 
   useEffect(() => {
     const loadProvider = async () => {
@@ -39,20 +42,44 @@ function App() {
     const loadBalance = async () => {
       const { contract, web3 } = web3Api
       const balance = await web3.eth.getBalance(contract.address)
-      setBalance(web3.utils.fromWei(balance, "ether"))
+      setBallance(web3.utils.fromWei(balance, "ether"))
     }
 
     web3Api.contract && loadBalance()
-  }, [web3Api])  
+  }, [web3Api, shouldReload])
 
   useEffect(() => {
     const getAccount = async () => {
-      web3Api.web3.eth.getAccounts((error, accounts) => {
-        accounts && setAccount(accounts[0])
-      });
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
     }
+
     web3Api.web3 && getAccount()
-  }, [web3Api.web3]);
+  }, [web3Api.web3])
+
+
+  const addFunds = useCallback(async () => {
+    const { contract, web3 } = web3Api
+    await contract.addFunds({
+      from: account,
+      value: web3.utils.toWei("1", "ether")
+    })
+
+    // window.location.reload();
+    reloadEffect();
+  }, [web3Api, account, reloadEffect])
+
+  const withDraw = async () => {
+    const { contract, web3 } = web3Api
+    const withdrawAmount = web3.utils.toWei("0.1", "ether")
+    await contract.withdraw(withdrawAmount, {
+      from: account
+    })
+
+    // window.location.reload();
+    reloadEffect();
+  }
+  
 
   return (
     <>
@@ -78,9 +105,13 @@ function App() {
             Current Balance: <strong>{balance}</strong> ETH
           </div>
           <button
-            className="button is-link mr-2">Donate</button>
+            onClick={addFunds}
+            className="button is-link mr-2">
+              Donate 1eth
+            </button>
           <button
-            className="button is-primary">Withdraw</button>
+            onClick={withDraw}
+            className="button is-primary">Withdraw 1 eth</button>
         </div>
       </div>
     </>
